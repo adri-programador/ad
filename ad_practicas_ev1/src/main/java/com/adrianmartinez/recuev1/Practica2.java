9cbc1cd55659dee13f56e5338c784479ed3d01aa
a <a@a.com>
2022-02-03 22:38:54 +0100

    Refactorized Practica2 main method.

--- ad_practicas_ev1/src/main/java/com/adrianmartinez/recuev1/Practica2.java
+++ ad_practicas_ev1/src/main/java/com/adrianmartinez/recuev1/Practica2.java
@@ -6,35 +6,37 @@ import java.util.Scanner;
 
 public class Practica2 {
     
-    public static void start() throws FileNotFoundException, IOException{
-        String opciones = "";
-        do{   
-            System.out.println("\n\n--Practica2--");
-            System.out.println("[1] ejercicio1");
-            System.out.println("[2] ejercicio3");
-            System.out.println("[3] ejercicio4");
-            System.out.println("[4] ejercicio5");
-            System.out.println("[5] ejercicio6");
-            System.out.println("[6] volver al menu");
-            System.out.println("Elija una opción: ");
-            Scanner sc = new Scanner(System.in);
-            opciones = sc.next(); // recojo el valor del próximo caracter .next / nextLine recoge info de toda la linea
+    public static void start() throws IOException{
+        int opcion;
+        Scanner sc = new Scanner(System.in);
+        do{
+            do {
+                System.out.println("\n\n--Practica2--");
+                System.out.println("[1] ejercicio1");
+                System.out.println("[2] ejercicio3");
+                System.out.println("[3] ejercicio4");
+                System.out.println("[4] ejercicio5");
+                System.out.println("[5] ejercicio6");
+                System.out.println("[6] volver al menu");
+                System.out.println("Elija una opción: ");
+                opcion = sc.nextInt();
+            } while (opcion < 1 || opcion > 6);
 
-            switch(opciones){
-                case "1": 
+            switch(opcion){
+                case 1:
                     Practica2Ej1.ejercicio1();
                     break;
-                case "2":
+                case 2:
                     Practica2Ej3.ejercicio3();
                     break;
-                case "3":
+                case 3:
                     Practica2Ej3.ejercicio4();
                     break;
-                case "5":
+                case 4:
                     break;
-                case "6":
+                case 5:
                     break;
             }
-        } while(Integer.parseInt(opciones)<=6);
+        } while(opcion != 6);
     }
 }
